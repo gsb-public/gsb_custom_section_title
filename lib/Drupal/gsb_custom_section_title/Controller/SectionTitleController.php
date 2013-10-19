9cd713863ebd98e31ddb974a54917e32c32776bd
Tim Plunkett <git@plnktt.com>
2013-10-18 17:58:04 -0700

    Convert delete controller

--- lib/Drupal/gsb_custom_section_title/Controller/SectionTitleController.php
+++ lib/Drupal/gsb_custom_section_title/Controller/SectionTitleController.php
@@ -7,6 +7,8 @@
 
 namespace Drupal\gsb_custom_section_title\Controller;
 
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\RemoveCommand;
 use Drupal\Core\Controller\ControllerBase;
 
 /**
@@ -22,6 +24,36 @@ class SectionTitleController extends ControllerBase {
       'table' => drupal_get_form('Drupal\gsb_custom_section_title\Form\SectionTitleTable'),
       'section' => drupal_get_form('Drupal\gsb_custom_section_title\Form\SectionTitleForm', $section_id),
     );
+  }
+
+  /**
+   * Deletes a section title.
+   *
+   * @param int $id
+   *   The unique identifier for this row.
+   * @param string $token
+   *   The secure token.
+   * @param bool $js
+   *   Whether this form was submitted via AJAX or not. Defaults to FALSE.
+   *
+   * @return \Drupal\Core\Ajax\AjaxResponse
+   */
+  public function deleteSection($id, $token, $js) {
+    if (\Drupal::csrfToken()->validate($token, $id)) {
+      $sections = $this->config('gsb_custom_section_title.settings')->get('sections');
+      unset($sections[$id]);
+      gsb_custom_section_title_set_sections($sections);
+      if ($js) {
+        // If there are no more sections, remove the whole table.
+        if (empty($sections)) {
+          $id = 'table';
+        }
+        $response = new AjaxResponse();
+        $response->addCommand(new RemoveCommand("#gsb-custom-section-title-$id"));
+        return $response;
+      }
+    }
+    return $this->redirect('gsb_custom_section_title.configure');
   }
 
 }
